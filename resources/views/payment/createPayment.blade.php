db22e3e2ea9069e6d92a84b17c4ff2770b5225d5
mohamedsafi7 <mohamedsafi.texting@gmail.com>
2024-03-16 16:09:52 +0000

    commit

--- resources/views/payment/createPayment.blade.php
+++ resources/views/payment/createPayment.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="w-full max-w-[300px] bg-white rounded-lg shadow-md p-6">
-        <h2 class="text-2xl font-bold text-gray-800 mb-4">Add a Payment </h2>
+        <h2 class="text-2xl font-bold text-gray-800 mb-4">Add a Payment : </h2>
         <form action="{{ route('payments.store') }}" method="POST" class="flex flex-col">
             @csrf
             <input type="text" class="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" id="amount" name="amount" placeholder="amount">
