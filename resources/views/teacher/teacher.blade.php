2daa92a6eecd71f8010b7c049a9712a9f3e80b19
mohamedsafi7 <mohamedsafi.texting@gmail.com>
2024-03-18 18:24:01 +0000

    modal

--- resources/views/teacher/teacher.blade.php
+++ resources/views/teacher/teacher.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
 <link rel="stylesheet" href="css/add.css">
+<link
+href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+rel="stylesheet"
+/>
+<link rel="stylesheet" href="../../assets/css/tailwind.output.css" />
+<script
+src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+defer
+></script>
+<script src="../../assets/js/init-alpine.js"></script>
+<!-- You need focus-trap.js to make the modal accessible -->
+<script src="../../assets/js/focus-trap.js" defer></script>
 @section('content')
 <div class="w-full overflow-x-auto">
     <table class="w-full whitespace-no-wrap">
@@ -44,11 +56,12 @@
                 </td>
                 <td class="px-4 py-3">
                     <div class="flex items-center space-x-4 text-sm">
-                        <button class="edit-Teacher-modal flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit" data-toggle="modal" data-target="#editTeacherModal" data-Teacher-id="{{$rs->id}}" data-Teacher-name="{{$rs->name}}" data-Teacher-cin="{{$rs->cin}}" data-Teacher-phone="{{$rs->phone}}" data-Teacher-email="{{$rs->email}}" data-Teacher-dob="{{$rs->date_naissance}}" data-Teacher-class="{{$rs->classe_id}}">
-                            <svg class="w-5 h-5" aria-hidden="true"  fill="currentColor" viewBox="0 0 20 20">
+                        <button @click="openModal" onclick="openEditModal('{{ $rs->name }}', '{{ $rs->cin }}', '{{ $rs->phone }}', '{{ $rs->email }}', '{{ $rs->dateN }}')" class="edit-Teacher-modal flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                             </svg>
                         </button>
+                        
                         <button class="show-Teacher-modal flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Show" data-toggle="modal" data-target="#TeacherModal" data-Teacher-name="{{$rs->name}}" data-Teacher-cin="{{$rs->cin}}" data-Teacher-phone="{{$rs->phone}}" data-Teacher-email="{{$rs->email}}" data-Teacher-dob="{{$rs->date_naissance}}" data-Teacher-class="{{$rs->classe_id}}">
                             <i class="fa-solid fa-eye"></i>
                         </button>
@@ -78,5 +91,98 @@
         @endif
       </tbody>
     </table>
+    <div
+    x-show="isModalOpen"
+    x-transition:enter="transition ease-out duration-150"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-150"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0"
+    class="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
+  >
+    <!-- Modal -->
+    <div
+      x-show="isModalOpen"
+      x-transition:enter="transition ease-out duration-150"
+      x-transition:enter-start="opacity-0 transform translate-y-1/2"
+      x-transition:enter-end="opacity-100"
+      x-transition:leave="transition ease-in duration-150"
+      x-transition:leave-start="opacity-100"
+      x-transition:leave-end="opacity-0  transform translate-y-1/2"
+      @click.away="closeModal"
+      @keydown.escape="closeModal"
+      class="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl"
+      role="dialog"
+      id="modal"
+    >
+      <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. -->
+      <header class="flex justify-end">
+        <button
+          class="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
+          aria-label="close"
+          @click="closeModal"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            role="img"
+            aria-hidden="true"
+          >
+            <path
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </header>
+      <!-- Modal body -->
+      <div class="mt-4 mb-6">
+        <!-- Modal title -->
+        <p
+          class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300"
+        >
+          Edit Teacher
+        </p>
+        <!-- Modal description -->
+            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" type="text" placeholder="name" value="{{ $rs-> name }}"></br>
+            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" type="text" placeholder="cin" value="{{ $rs-> cin }}"></br>
+            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" type="text" placeholder="phone" value="{{ $rs-> phone }}"></br>
+            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" type="text" placeholder="email" value="{{ $rs-> email }}"></br>   
+            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" type="date" placeholder="dateN" value="{{ $rs-> dateN }}">
+      </div>
+      <footer
+        class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800"
+      >
+        <button
+          @click="closeModal"
+          class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
+        >
+          Cancel
+        </button>
+        <button
+          class="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        >
+          Accept
+        </button>
+      </footer>
+    </div>
   </div>
+  </div>
+  <script>
+    function openEditModal(name, cin, phone, email, dateN) {
+    // Populate input fields with teacher's information
+    document.getElementById('teacherName').value = name;
+    document.getElementById('teacherCin').value = cin;
+    document.getElementById('teacherPhone').value = phone;
+    document.getElementById('teacherEmail').value = email;
+    document.getElementById('teacherDateN').value = dateN;
+
+    // Open the modal
+    $('#editTeacherModal').modal('show');
+}
+
+</script>
 @endsection
