f5491a71989f1bab0b4c93a4faeb7595fbf9f169
mohamedsafi7 <mohamedsafi.texting@gmail.com>
2024-03-27 20:59:51 +0000

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,18 +7,32 @@ use App\Models\Formation;
 use App\Models\Student;
 use App\Models\Teacher;
 use App\Models\Payment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
 {
+    $lastStudent = Student::latest()->first();
+    $lastTeacher = Teacher::latest()->first();
+    $lastClass = Classe::latest()->first();
+    $lastPayment = Payment::latest()->first();
+
     $totalStudents = Student::count();
     $totalTeachers = Teacher::count();
     $totalClasses = Classe::count();
     $totalAmount = Payment::sum('amount');
     $totalFormations = Formation::count();
+    
+    $students = Student::all();
+    $totalAge = 0;
+    foreach ($students as $student) {
+        $totalAge += Carbon::parse($student->date_naissance)->age;
+    }
+    $averageAge = $totalStudents > 0 ? round($totalAge / $totalStudents) : 0;
 
-    return view('welcome', compact('totalStudents', 'totalTeachers', 'totalClasses','totalFormations','totalAmount'));
+
+    return view('welcome', compact( 'averageAge','totalStudents', 'totalTeachers', 'totalClasses','totalFormations','totalAmount','lastStudent','lastTeacher','lastClass','lastPayment'));
 }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,81 +111,220 @@
               {{ $totalFormations }}
               </p>
             </div>
-          </div>
-        
             
+          </div>
+          <!-- Card -->
+          <div
+            class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"
+          >
+            <div
+              class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500"
+            >
+              <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                ></path>
+              </svg>
+            </div>
+            <div>
+              <p
+                class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
+              >
+                total Classes
+              </p>
+              <p
+                class="text-lg font-semibold text-gray-700 dark:text-gray-200"
+              >
+              {{ $totalClasses }}
+              </p>
+            </div>
+            
+          </div>
+          <!-- Card -->
+<div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+  <div class="p-3 mr-4 text-indigo-500 bg-indigo-100 rounded-full dark:text-indigo-100 dark:bg-indigo-500">
+      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+          <path fill-rule="evenodd" d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" clip-rule="evenodd"></path>
+      </svg>
+  </div>
+  <div>
+      <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          Average Age of Students
+      </p>
+      <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+          {{ $averageAge }} years
+      </p>
+  </div>
+</div>
+
           </div>
         </div>
         <div style="background-image: url('{{ asset('col (2).jpg') }}'); background-size: cover; background-position: center; height: 630px; position: relative;">
           <h1 style="text-align: center; color: rgb(255, 255, 255); font-weight: bold; font-size: 24px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background-color: rgba(0, 0, 0, 0.5);">WYKEE est un centre de formation professionnelle innovant, Complète et 100% pratique avec des pédagogies d'apprentissage innovants, de plusieurs domaines clés tels que l'informatique, le football, le développement personnel.</h1>
         </div>
+
+        
         <!-- Additional content below the background image -->
-        <div class="my-8">
-          <div class="container mx-auto">
-            <h2 class="text-3xl font-bold text-center text-gray-800 dark:text-gray-200 mb-6">Why Choose WYKEE?</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <div class="flex flex-col justify-center items-center bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 104.8c0-9.2-5.8-17.3-13.2-22.8C167.2 73.3 160 61.3 160 48c0-26.5 28.7-48 64-48s64 21.5 64 48c0 13.3-7.2 25.3-18.8 34c-7.4 5.5-13.2 13.6-13.2 22.8c0 12.8 10.4 23.2 23.2 23.2H336c26.5 0 48 21.5 48 48v56.8c0 12.8 10.4 23.2 23.2 23.2c9.2 0 17.3-5.8 22.8-13.2c8.7-11.6 20.7-18.8 34-18.8c26.5 0 48 28.7 48 64s-21.5 64-48 64c-13.3 0-25.3-7.2-34-18.8c-5.5-7.4-13.6-13.2-22.8-13.2c-12.8 0-23.2 10.4-23.2 23.2V464c0 26.5-21.5 48-48 48H279.2c-12.8 0-23.2-10.4-23.2-23.2c0-9.2 5.8-17.3 13.2-22.8c11.6-8.7 18.8-20.7 18.8-34c0-26.5-28.7-48-64-48s-64 21.5-64 48c0 13.3 7.2 25.3 18.8 34c7.4 5.5 13.2 13.6 13.2 22.8c0 12.8-10.4 23.2-23.2 23.2H48c-26.5 0-48-21.5-48-48V343.2C0 330.4 10.4 320 23.2 320c9.2 0 17.3 5.8 22.8 13.2C54.7 344.8 66.7 352 80 352c26.5 0 48-28.7 48-64s-21.5-64-48-64c-13.3 0-25.3 7.2-34 18.8C40.5 250.2 32.4 256 23.2 256C10.4 256 0 245.6 0 232.8V176c0-26.5 21.5-48 48-48H168.8c12.8 0 23.2-10.4 23.2-23.2z"/></svg>                  </svg>
-                </div>
-                <div class="text-center mt-4">
-                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                    Expert Instructors
-                  </p>
-                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                    Content for Expert Instructors
-                  </p>
-                </div>
-              </div>
-              <div class="flex flex-col justify-center items-center bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M78.6 5C69.1-2.4 55.6-1.5 47 7L7 47c-8.5 8.5-9.4 22-2.1 31.6l80 104c4.5 5.9 11.6 9.4 19 9.4h54.1l109 109c-14.7 29-10 65.4 14.3 89.6l112 112c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-112-112c-24.2-24.2-60.6-29-89.6-14.3l-109-109V104c0-7.5-3.5-14.5-9.4-19L78.6 5zM19.9 396.1C7.2 408.8 0 426.1 0 444.1C0 481.6 30.4 512 67.9 512c18 0 35.3-7.2 48-19.9L233.7 374.3c-7.8-20.9-9-43.6-3.6-65.1l-61.7-61.7L19.9 396.1zM512 144c0-10.5-1.1-20.7-3.2-30.5c-2.4-11.2-16.1-14.1-24.2-6l-63.9 63.9c-3 3-7.1 4.7-11.3 4.7H352c-8.8 0-16-7.2-16-16V102.6c0-4.2 1.7-8.3 4.7-11.3l63.9-63.9c8.1-8.1 5.2-21.8-6-24.2C388.7 1.1 378.5 0 368 0C288.5 0 224 64.5 224 144l0 .8 85.3 85.3c36-9.1 75.8 .5 104 28.7L429 274.5c49-23 83-72.8 83-130.5zM56 432a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>                  </svg>
-                </div>
-                <div class="text-center mt-4">
-                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                    Practical Learning
-                  </p>
-                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                    Content for Practical Learning
-                  </p>
-                </div>
-              </div>
-              <div class="flex flex-col justify-center items-center bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h56 64 16c17.7 0 32-14.3 32-32s-14.3-32-32-32H384V64H576V256H384V224H320v48c0 26.5 21.5 48 48 48H592c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H368c-26.5 0-48 21.5-48 48v80H243.1 177.1c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9V480c0 17.7 14.3 32 32 32s32-14.3 32-32z"/></svg>                  </svg>
-                </div>
-                <div class="text-center mt-4">
-                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                    Customized Programs
-                  </p>
-                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                    Content for Customized Programs
-                  </p>
-                </div>
-              </div>
-              
-              <div class="flex flex-col justify-center items-center bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h56 64 16c17.7 0 32-14.3 32-32s-14.3-32-32-32H384V64H576V256H384V224H320v48c0 26.5 21.5 48 48 48H592c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H368c-26.5 0-48 21.5-48 48v80H243.1 177.1c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9V480c0 17.7 14.3 32 32 32s32-14.3 32-32z"/></svg>                  </svg>
-                </div>
-                <div class="text-center mt-4">
-                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                    Expert Instructors
-                  </p>
-                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                    Content for Expert Instructors
-                  </p>
-                </div>
-              </div>
-            </div>
+        <div class="grid gap-6 mb-8 md:grid-cols-2">
+          <!-- Latest Student Table -->
+          <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+              <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                  Latest Student
+              </h4>
+              <table class="w-full whitespace-no-wrap">
+                  <!-- Table headers -->
+                  <!-- Modify these according to your student attributes -->
+                  <thead>
+                      <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                          <th class="px-4 py-3">ID</th>
+                          <th class="px-4 py-3">Name</th>
+                          <th class="px-4 py-3">Phone</th>
+                          <th class="px-4 py-3">Email</th>
+                          <th class="px-4 py-3">Date of Birth</th>
+                      </tr>
+                  </thead>
+                  <!-- Table body -->
+                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                      @if($lastStudent)
+                          <tr class="text-gray-700 dark:text-gray-400">
+                              <!-- Populate these with the corresponding student data -->
+                              <td>{{ $lastStudent->id }}</td>
+                              <td class="px-4 py-3">{{ $lastStudent->name }}</td>
+                              <td class="px-4 py-3">{{ $lastStudent->phone }}</td>
+                              <td class="px-4 py-3">{{ $lastStudent->email }}</td>
+                              <td class="px-4 py-3">{{ $lastStudent->date_naissance }}</td>
+                          </tr>
+                      @else
+                          <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                              <td class="px-4 py-3" colspan="5">Student Not Found</td>
+                          </tr>
+                      @endif
+                  </tbody>
+              </table>
+          </div>
+  
+          <!-- Latest Teacher Table -->
+          <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+              <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                  Latest Teacher
+              </h4>
+              <table class="w-full whitespace-no-wrap">
+                  <!-- Table headers -->
+                  <!-- Modify these according to your teacher attributes -->
+                  <thead>
+                      <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                          <th class="px-4 py-3">ID</th>
+                          <th class="px-4 py-3">Name</th>
+                          <th class="px-4 py-3">Phone</th>
+                          <th class="px-4 py-3">Email</th>
+                          <th class="px-4 py-3">Date of Birth</th>
+                      </tr>
+                  </thead>
+                  <!-- Table body -->
+                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                      @if($lastTeacher)
+                          <tr class="text-gray-700 dark:text-gray-400">
+                              <!-- Populate these with the corresponding teacher data -->
+                              <td>{{ $lastTeacher->id }}</td>
+                              <td class="px-4 py-3">{{ $lastTeacher->name }}</td>
+                              <td class="px-4 py-3">{{ $lastTeacher->phone }}</td>
+                              <td class="px-4 py-3">{{ $lastTeacher->email }}</td>
+                              <td class="px-4 py-3">{{ $lastTeacher->date_of_birth }}</td>
+                          </tr>
+                      @else
+                          <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                              <td class="px-4 py-3" colspan="5">Teacher Not Found</td>
+                          </tr>
+                      @endif
+                  </tbody>
+              </table>
           </div>
         </div>
-                
 
+        <div class="grid gap-6 mb-8 md:grid-cols-2">
+          <!-- Latest cLASS Table -->
+          <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+              <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                  Latest Class
+              </h4>
+              <table class="w-full whitespace-no-wrap">
+                  <!-- Table headers -->
+                  <!-- Modify these according to your cLASS attributes -->
+                  <thead>
+                      <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                          <th class="px-4 py-3">ID</th>
+                          <th class="px-4 py-3">Name</th>
+                          <th class="px-4 py-3">Teacher</th>
+                          <th class="px-4 py-3">Formation</th>
+                      </tr>
+                  </thead>
+                  <!-- Table body -->
+                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                      @if($lastClass)
+                          <tr class="text-gray-700 dark:text-gray-400">
+                              <!-- Populate these with the corresponding student data -->
+                              <td>{{ $lastClass->id }}</td>
+                              <td class="px-4 py-3">{{ $lastClass->name }}</td>
+                              <td class="px-4 py-3">{{ $lastClass->teacher->name }}</td>
+                              <td class="px-4 py-3">{{ $lastClass->formation->name }}</td>
+                          </tr>
+                      @else
+                          <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                              <td class="px-4 py-3" colspan="5">Class Not Found</td>
+                          </tr>
+                      @endif
+                  </tbody>
+              </table>
+          </div>
+  
+          <!-- Latest Payment Table -->
+          <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+              <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                  Latest Payment
+              </h4>
+              <table class="w-full whitespace-no-wrap">
+                  <!-- Table headers -->
+                  <!-- Modify these according to your Payment attributes -->
+                  <thead>
+                      <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                          <th class="px-4 py-3">ID</th>
+                          <th class="px-4 py-3">student</th>
+                          <th class="px-4 py-3">amount</th>
+                          <th class="px-4 py-3">date</th>
+                          <th class="px-4 py-3">status </th>
+                      </tr>
+                  </thead>
+                  <!-- Table body -->
+                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                      @if($lastPayment)
+                          <tr class="text-gray-700 dark:text-gray-400">
+                              <!-- Populate these with the corresponding Payment data -->
+                              <td>{{ $lastPayment->id }}</td>
+                              <td class="px-4 py-3">{{ $lastPayment->student->name }}</td>
+                              <td class="px-4 py-3">{{ $lastPayment->amount }}</td>
+                              <td class="px-4 py-3">{{ $lastPayment->date }}</td>
+                              
+                              <td class="px-4 py-3">
+                                @if($lastPayment->status == 'paid')
+                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                    Paid
+                                </span>
+                            @elseif($lastPayment->status == 'unpaid')
+                                <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
+                                    Unpaid
+                                </span>
+                            @endif
+                          </tr>
+                      @else
+                          <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                              <td class="px-4 py-3" colspan="5">Payment Not Found</td>
+                          </tr>
+                      @endif
+                  </tbody>
+              </table>
+              
+          </div>
+          
       </div>
-    @endsection
-
+  </div>
+  @endsection
